b1e58bafd385ead0426e31251940f32c8a990968
matheus.sampaio <136608061+Tomatin04@users.noreply.github.com>
2025-05-02 17:55:16 -0300

    A
    a

---
+++ Front-End/src/view/register_view.php
@@ -0,0 +1,25 @@
+<?php require_once __DIR__ . "/header.php"?>
+
+<main>
+        <div class="login-container">
+            <h2>Cadastrar</h2>
+            <form action="#" method="POST">
+                <label for="nome">Nome:</label>
+                <input type="text" id="nome" name="nome" required>
+                
+                <label for="email">E-mail:</label>
+                <input type="email" id="email" name="email" required>
+
+                <label for="senha">Senha:</label>
+                <input type="password" id="senha" name="senha" required>
+
+                <label for="confirmar-senha">Confirmar Senha:</label>
+                <input type="password" id="confirmar-senha" name="confirmar-senha" required>
+
+                <button type="submit">Cadastrar</button>
+            </form>
+            <p>Já tem uma conta? <a href="#">Faça login aqui</a></p>
+        </div>
+    </main>
+
+<?php require_once __DIR__ . "/footer.php"?>
